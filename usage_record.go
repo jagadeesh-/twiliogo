a8622f934c36b631121e9b436f6f67d76977a5e3
jagadeeshm <jagadeesh.hya@gmail.com>
2016-06-06 16:57:13 -0400

    Usage Resource Price datatype modified to float64

--- usage_record.go
+++ usage_record.go
@@ -1,20 +1,17 @@
 package twiliogo
 
-
-
 type UsageRecord struct {
-    Category        string `json:"category"`
-    Description     string `json:"description"`
-    AccountSid      string `json:"account_sid"`
-    StartDate       string `json:"start_date"`
-    EndDate         string `json:"end_date"`
-    Usage           string  `json:"usage"`
-    UsageUnit       string  `json:"usage_unit"`
-    Count           string  `json:"count"`
-    CountUnit       string  `json:"count_unit"`
-    Price           string  `json:"price"`
-    PriceUnit       string  `json:"price_unit"`
-    Uri             string  `json:"uri"`
-    SubresourceUris string  `json:"sub_resource_uris"` 
+	Category        string  `json:"category"`
+	Description     string  `json:"description"`
+	AccountSid      string  `json:"account_sid"`
+	StartDate       string  `json:"start_date"`
+	EndDate         string  `json:"end_date"`
+	Usage           string  `json:"usage"`
+	UsageUnit       string  `json:"usage_unit"`
+	Count           string  `json:"count"`
+	CountUnit       string  `json:"count_unit"`
+	Price           float64 `json:"price"`
+	PriceUnit       string  `json:"price_unit"`
+	Uri             string  `json:"uri"`
+	SubresourceUris string  `json:"sub_resource_uris"`
 }
-
